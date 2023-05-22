d98004197fa999042471ea59f198680f1e9fa1b3
dlgpwjd7273@naver.com <dlgpwjd7273@naver.com>
2023-05-22 16:43:55 +0900

    5강 : 싱글톤 컨테이너 - 6. @Configuration과 싱글톤 @Configuration과 바이트코드 조작의 마법 스프링 설정 정보 클래스는 @CGLIB 라이브러리를 통해서 상속받은 임의의 다른 클래스를 만들어서 스프링 빈으로 등록함. -> 그 역할을 @Configuration가 해줘서 없으면 스프링 빈으로 등록은 되지만, 싱글톤이 보장되지 않아서 스프링 컨테이너가 관리해주지 않게 됨.

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -17,8 +17,6 @@ public class ConfigurationSingletonTest {
     void configurationTest() {
         ApplicationContext ac = new AnnotationConfigApplicationContext((AppConfig.class));
 
-        // 테스트 용도로 만들어둔 것 메서드를 사용하기 위해서 타입을 Impl로 지정
-        // 원래 구체 타입으로 사용하면 안 좋음
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
         MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
@@ -26,12 +24,19 @@ public class ConfigurationSingletonTest {
         MemberRepository memberRepository1 = memberService.getMemberRepository();
         MemberRepository memberRepository2 = orderService.getMemberRepository();
         
-        // 같은 인스턴스가 조회됨
         System.out.println("memberService -> memberRepository = " + memberRepository1);
         System.out.println("orderService -> memberRepository = " + memberRepository2);
         System.out.println("memberRepository = " + memberRepository);
 
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext((AppConfig.class));
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass()); // bean = class hello.core.AppConfig$$SpringCGLIB$$0
     }
 }
